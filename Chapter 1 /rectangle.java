dabdfca583c09af5b6746d5d0301377cde57831c
404kevnotfound <chloee.stephenson@suusuccess.org>
2018-10-11 09:33:42 -0600

    Added Chapter 1 Assignments

---
+++ Chapter 1 /rectangle.java
@@ -0,0 +1,12 @@
+/*
+Chloee 
+9/25/18
+Area and perimeter of a rectangle
+*/
+public class rectangle{
+	public static void main(String[] args) {
+		System.out.println((2 * 4.5) + (2 * 7.9));
+		System.out.print(4.5 * 7.9);
+	}
+
+}
